1d7f9a6bfc4b1867363ce26a856cb38050fd23a4
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-03-09 11:15:04 +0100

    dailyforecast, changed style placeholder, added div in html

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ async function loadWeatherData(cityName) {
     let response = await fetch(URL);
     let data = await response.json();
     renderMainHTML(data);
-    renderForecastHTML(data);
+    renderHourlyForecastHTML(data);
+    renderDailyForecastHTML(data);
 }
 
 
@@ -21,32 +22,30 @@ function handleFormSubmit(event) {
 function renderMainHTML(data){
     let dateObj = new Date(data.location.localtime);
 
-    // Extrahieren der Datumsteile
     let day = String(dateObj.getDate()).padStart(2, '0');
-    let month = String(dateObj.getMonth() + 1).padStart(2, '0'); // Monate beginnen bei 0
+    let month = String(dateObj.getMonth() + 1).padStart(2, '0'); 
     let year = dateObj.getFullYear();
 
-    // Extrahieren der Zeitteile
     let hours = String(dateObj.getHours()).padStart(2, '0');
     let minutes = String(dateObj.getMinutes()).padStart(2, '0');
 
-    // Zusammenfügen des Datums im gewünschten Format
     let myDate = `${day}.${month}.${year} - ${hours}:${minutes}`;
 
     document.getElementById('main-section').innerHTML = '';
     document.getElementById('main-section').innerHTML = /*html*/ `
-    <h3>${data.location.name}</h3>
-    <p>${data.location.country}</p>
-    <p>Temperature: ${data.current.temp_c}°C</p>
-    <p>Wind: ${data.current.wind_kph}km/h</p>
-    <p>Humidity: ${data.current.humidity}%</p>
-    <p>Condition: ${data.current.condition.text}</p>
-    <img src="${data.current.condition.icon}" alt="weather icon">
-    <p>Last updated: ${myDate} local time</p>
-    `;
+    <div class="weather-data">
+        <h3>${data.location.name}</h3>
+        <p>${data.location.country}</p>
+        <p>Temperature: ${data.current.temp_c}°C</p>
+        <p>Wind: ${data.current.wind_kph}km/h ${data.current.wind_dir}</p>
+        <p>Humidity: ${data.current.humidity}%</p>
+        <p>Condition: ${data.current.condition.text}</p>
+        <img src="${data.current.condition.icon}" alt="weather icon">
+        <p>Last updated: ${myDate} local time</p>
+    </div>`;
 }
 
-function renderForecastHTML(data) {
+function renderHourlyForecastHTML(data) {
     document.getElementById('forecast-section').innerHTML = '';
 
     data.forecast.forecastday[0].hour.forEach(hour => {
@@ -54,15 +53,26 @@ function renderForecastHTML(data) {
         let hours = String(dateObj.getHours()).padStart(2, '0');
         let minutes = String(dateObj.getMinutes()).padStart(2, '0');
 
-        // Überprüfung, ob die Stunde 06:00, 12:00, 18:00 oder 00:00 entspricht
         if (hours === '06' || hours === '12' || hours === '18' || hours === '00') {
             document.getElementById('forecast-section').innerHTML += /*html*/ `
             <div class="hourly-forecast">
                 <p>${hours}:${minutes}</p>
                 <img src="${hour.condition.icon}" alt="weather icon">
                 <p>${hour.temp_c}°C</p>
-            </div>
-            `;
+            </div>`;
         }
     });
+}
+
+function renderDailyForecastHTML(data) {
+    document.getElementById('daily-forecast-section').innerHTML = '';
+
+    data.forecast.forecastday.forEach(day => {
+        document.getElementById('daily-forecast-section').innerHTML += /*html*/ `
+        <div class="daily-forecast">
+            <p>${day.date}</p>
+            <img src="${day.day.condition.icon}" alt="weather icon">
+            <p>Max / Min: ${day.day.maxtemp_c}°C / ${day.day.mintemp_c}°C</p>
+        </div>`;
+    })
 }
